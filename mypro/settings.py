e5a15beb4f8d0bd563242ff263d62205f9b0f247
liam <m15kh10@email.com>
2023-09-17 02:16:09 +0330

    ch5

--- mypro/settings.py
+++ mypro/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #local app
     'accounts.apps.AccountsConfig',
+    'pages.apps.PagesConfig',
 ]
 
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'mypro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
